52392c6ea80724f08ed7bd26f4d08499cfbe2546
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-09-05 14:17:44 -0300

    feat: start project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./layout/Header"
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <h1>Hello World</h1>
+      <p>Olá mundo</p>
+    </>
+  )
+}
+
+export default App
